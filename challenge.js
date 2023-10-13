3a6eae37de12a26938dcc4c74c120df9b247cbdf
sharmakeHussein <s.mohamud2@gmail.com>
2023-10-13 19:40:40 +0100

    for loop while loop

---
+++ challenge.js
@@ -0,0 +1,67 @@
+// write a for loop that prints your first name 10 times
+
+for (let i = 0; i < 10; i++) {
+    //console.log("sharmake")
+}
+
+// write a while loop that prints your surname 15 
+
+i = 0
+while (i < 10) {
+    //console.log("hussein")
+    i++
+    
+}
+
+// write a for loop that iterates through each element and prints each element
+
+
+let students = ["john", "layland", "ali", "Bushra", "ahmed", "liban", "noora", "hida"]
+
+let cities = ["west bank","gaza"]
+let country = ["falestine"]
+
+for (let i = 0; i < students.length; i++) {
+    console.log(students[i])
+    
+}
+
+var i = 0
+do {
+    console.log("sharmake");
+    i++
+}
+while (i < 20)
+
+// write an ainfinit for loop
+
+for (;;) {
+    console.log('hello')
+}
+
+
+
+// write an infinite loop using a while loop
+while(true) {
+    console.log("hello 2");
+} 
+
+let days = "Monday"
+if (days == "Monday") {
+    console.log("it is work time")
+} else if (day == "Tuesday") {
+    console.log("it is school time");
+}
+else {
+    console.log("it is off time");
+}
+
+let day = "Monday"
+if (day == "Monday") {
+    console.log("its work time");
+} else if (day == "Friday") {
+    console.log("its  day off");  
+}
+else {
+    console.log("it is off time");
+}
